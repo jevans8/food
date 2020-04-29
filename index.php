0fd6b52565729bd648b24f46a62ce06886dd82f8
Julia Evans <jevans8@mail.greenriver.edu>
2020-04-29 17:33:02 -0400

    Added order functionality

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -26,12 +29,10 @@ $f3->route('GET /', function(){
 //Breakfast route
 $f3->route('GET /breakfast', function(){
 
-    //echo "<h1>Welcome to my breakfast page</h1>";
-
     //instantiate new template object
     $view = new Template();
 
-    //display home page via render method
+    //display page via render method
     echo $view->render('views/bfast.html');
 });
 
@@ -41,21 +42,67 @@ $f3->route('GET /breakfast/greenEggs', function(){
     //instantiate new template object
     $view = new Template();
 
-    //display home page via render method
+    //display page via render method
     echo $view->render('views/greenEggsAndHam.html');
 });
 
 //Lunch route
 $f3->route('GET /lunch', function(){
 
-    //echo "<h1>Welcome to my breakfast page</h1>";
+    //instantiate new template object
+    $view = new Template();
+
+    //display page via render method
+    echo $view->render('views/lunch.html');
+});
+
+//Order route
+$f3->route('GET|POST /order', function($f3){
+
+    //if form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //var_dump($_POST); //array(2) { ["food"]=> string(5) "pizza" ["meal"]=> string(6) "dinner" }
+
+        //validate form data
+        $meals = array("breakfast", "lunch", "dinner");
+
+        if(empty($_POST['food']) || !in_array($_POST['meal'], $meals)){
+
+            echo "<p>Please enter a food and select a meal</p>";
+
+        }
+        //data is valid
+        else {
+
+            $_SESSION['food'] = $_POST['food'];
+            $_SESSION['meal'] = $_POST['meal'];
+
+            //redirect to order summary page
+            $f3->reroute('summary');
+
+            session_destroy();
+        }
+    }
 
     //instantiate new template object
     $view = new Template();
 
-    //display home page via render method
-    echo $view->render('views/lunch.html');
+    //display page via render method
+    echo $view->render('views/order.html');
 });
+
+//Order summary route
+$f3->route('GET /summary', function(){
+
+    //instantiate new template object
+    $view = new Template();
+
+    //display page via render method
+    echo $view->render('views/summary.html');
+
+});
+
 
 //Run F3
 $f3->run();
